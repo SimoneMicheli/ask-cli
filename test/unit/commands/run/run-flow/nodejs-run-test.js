4737d5d7d6b5a5e00f91d18b1eab830ff9fb69ab
Simone Micheli <s.micheli965@gmail.com>
2022-06-11 14:43:51 +0200

    feat: add support to Typescript files in ts-node environment

--- test/unit/commands/run/run-flow/nodejs-run-test.js
+++ test/unit/commands/run/run-flow/nodejs-run-test.js
@@ -15,7 +15,9 @@ describe('Node run flow test', () => {
         sinon.restore();
     });
     it('| validate nodemon config, run mode', () => {
-        sinon.stub(fs, 'existsSync').returns(true);
+        const stub = sinon.stub(fs, 'existsSync');
+        stub.returns(true);
+        stub.withArgs(path.join(process.cwd(), 'fooFolder', 'fooFileName.ts')).returns(false);
         const runFlow = new NodejsRunFlow({ skillInvocationInfo,
             waitForAttach: false,
             debugPort: CONSTANTS.RUN.DEFAULT_DEBUG_PORT,
@@ -41,7 +43,9 @@ describe('Node run flow test', () => {
         expect(nodemonConfig.watch).eq(false);
     });
     it('| validate nodemon config, debug mode, watch enabled', () => {
-        sinon.stub(fs, 'existsSync').returns(true);
+        const stub = sinon.stub(fs, 'existsSync');
+        stub.returns(true);
+        stub.withArgs(path.join(process.cwd(), 'fooFolder', 'fooFileName.ts')).returns(false);
         const runFlow = new NodejsRunFlow({ skillInvocationInfo,
             waitForAttach: true,
             debugPort: CONSTANTS.RUN.DEFAULT_DEBUG_PORT,
@@ -63,6 +67,66 @@ describe('Node run flow test', () => {
             'fooHandler',
             '--skillEntryFile',
             path.join(process.cwd(), 'fooFolder', 'fooFileName.js'),
+            '--region',
+            CONSTANTS.ALEXA.REGION.NA
+        ]);
+        expect(nodemonConfig.watch).eq('fooFolder');
+    });
+    it('| validate nodemon config, run mode [Typescript]', () => {
+        const stub = sinon.stub(fs, 'existsSync');
+        stub.returns(true);
+        stub.withArgs(path.join(process.cwd(), 'fooFolder', 'fooFileName.ts')).returns(true);
+        const runFlow = new NodejsRunFlow({ skillInvocationInfo,
+            waitForAttach: false,
+            debugPort: CONSTANTS.RUN.DEFAULT_DEBUG_PORT,
+            token: 'fooToken',
+            skillId: 'fooSkill',
+            runRegion: CONSTANTS.ALEXA.REGION.NA,
+            watch: false });
+        const nodemonConfig = runFlow.getExecConfig();
+        expect(nodemonConfig.execMap).to.deep.equal({
+            js: 'node -r ts-node/register'
+        });
+        expect(nodemonConfig.script).eq(path.join(process.cwd(), 'fooFolder', CONSTANTS.RUN.NODE.SCRIPT_LOCATION));
+        expect(nodemonConfig.args).to.deep.equal([
+            '--accessToken',
+            '"fooToken"',
+            '--skillId',
+            'fooSkill',
+            '--handlerName',
+            'fooHandler',
+            '--skillEntryFile',
+            path.join(process.cwd(), 'fooFolder', 'fooFileName.ts'),
+            '--region',
+            CONSTANTS.ALEXA.REGION.NA
+        ]);
+        expect(nodemonConfig.watch).eq(false);
+    });
+    it('| validate nodemon config, debug mode, watch enabled [Typescript]', () => {
+        const stub = sinon.stub(fs, 'existsSync');
+        stub.returns(true);
+        stub.withArgs(path.join(process.cwd(), 'fooFolder', 'fooFileName.ts')).returns(true);
+        const runFlow = new NodejsRunFlow({ skillInvocationInfo,
+            waitForAttach: true,
+            debugPort: CONSTANTS.RUN.DEFAULT_DEBUG_PORT,
+            token: 'fooToken',
+            skillId: 'fooSkill',
+            runRegion: CONSTANTS.ALEXA.REGION.NA,
+            watch: true });
+        const nodemonConfig = runFlow.getExecConfig();
+        expect(nodemonConfig.execMap).to.deep.equal({
+            js: `node -r ts-node/register --inspect-brk=${CONSTANTS.RUN.DEFAULT_DEBUG_PORT}`
+        });
+        expect(nodemonConfig.script).eq(path.join(process.cwd(), 'fooFolder', CONSTANTS.RUN.NODE.SCRIPT_LOCATION));
+        expect(nodemonConfig.args).to.deep.equal([
+            '--accessToken',
+            '"fooToken"',
+            '--skillId',
+            'fooSkill',
+            '--handlerName',
+            'fooHandler',
+            '--skillEntryFile',
+            path.join(process.cwd(), 'fooFolder', 'fooFileName.ts'),
             '--region',
             CONSTANTS.ALEXA.REGION.NA
         ]);
